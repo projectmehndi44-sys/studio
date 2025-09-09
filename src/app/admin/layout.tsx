4ea6a69a4f45b992cd837f0cf2d9bb97141365de
Abhishek <projectmehndi44@gmail.com>
2025-09-09 05:17:18 +0000

    activate scrolling in every page where required

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -194,7 +194,7 @@ function AdminLayoutContent({ children }: { children: React.ReactNode }) {
                         </DropdownMenuContent>
                     </DropdownMenu>
                 </header>
-                <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-muted/20">
+                <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-muted/20 overflow-auto">
                     {children}
                 </main>
             </div>
